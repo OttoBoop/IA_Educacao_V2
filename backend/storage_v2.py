891450bba97bed32f9bb26c5257cd4d571bcda1a
Otávio Bopp <80711736+OttoBoop@users.noreply.github.com>
2026-01-26 23:54:46 -0300

    Revert "Abrir e trocar documentos base na visualização da atividade"

--- backend/storage_v2.py
+++ backend/storage_v2.py
@@ -876,16 +876,7 @@ class StorageManagerV2:
         
         # Documentos base
         docs_base = [d for d in documentos if d.is_documento_base]
-        docs_base_detalhes = {}
-        for doc in docs_base:
-            tipo = doc.tipo.value
-            if tipo not in docs_base_detalhes:
-                docs_base_detalhes[tipo] = {
-                    "id": doc.id,
-                    "nome_arquivo": doc.nome_arquivo,
-                    "criado_em": doc.criado_em.isoformat()
-                }
-        tipos_base_existentes = [TipoDocumento(tipo) for tipo in docs_base_detalhes.keys()]
+        tipos_base_existentes = [d.tipo for d in docs_base]
         
         docs_base_faltando = []
         for tipo in [TipoDocumento.ENUNCIADO, TipoDocumento.GABARITO]:
@@ -914,10 +905,9 @@ class StorageManagerV2:
         return {
             "atividade": atividade.to_dict(),
             "documentos_base": {
-                "existentes": [tipo.value for tipo in tipos_base_existentes],
+                "existentes": [d.tipo.value for d in docs_base],
                 "faltando": docs_base_faltando,
-                "aviso": "Falta gabarito para poder corrigir" if TipoDocumento.GABARITO.value in docs_base_faltando else None,
-                "detalhes": docs_base_detalhes
+                "aviso": "Falta gabarito para poder corrigir" if TipoDocumento.GABARITO.value in docs_base_faltando else None
             },
             "alunos": {
                 "total": len(alunos),
